74d5eed686928798a9d9d3b15a118a757b10b321
AnSeonJeong <asj081@naver.com>
2022-05-20 18:21:11 +0900

    selected_category

---
+++ js/menu_category.js
@@ -0,0 +1,44 @@
+const menuBtn = document.querySelector(".menu button");
+const menuBox = document.querySelector(".menu .menu_box");
+const category = document.querySelectorAll(".menu .category li");
+const doughnutsBox = menuBox.querySelector(".doughnuts");
+const drinksBox = menuBox.querySelector(".drink");
+const icecreamBox = menuBox.querySelector(".ice");
+
+const HIDDEN = "hidden";
+
+menuBtn.addEventListener("click", () => {
+  menuBox.style.height = "auto";
+  menuBox.style.overflow = "visible";
+  menuBtn.classList.add(HIDDEN);
+});
+
+function showMenu(e) {
+  console.dir(e.target.innerText);
+  switch (e.target.innerText) {
+    case "전체":
+      doughnutsBox.classList.remove(HIDDEN);
+      drinksBox.classList.remove(HIDDEN);
+      icecreamBox.classList.remove(HIDDEN);
+      break;
+    case "도넛":
+      doughnutsBox.classList.remove(HIDDEN);
+      drinksBox.classList.add(HIDDEN);
+      icecreamBox.classList.add(HIDDEN);
+      break;
+    case "커피 / 음료":
+      drinksBox.classList.remove(HIDDEN);
+      doughnutsBox.classList.add(HIDDEN);
+      icecreamBox.classList.add(HIDDEN);
+      break;
+    case "아이스크림 / 프로즌":
+      icecreamBox.classList.remove(HIDDEN);
+      doughnutsBox.classList.add(HIDDEN);
+      drinksBox.classList.add(HIDDEN);
+      break;
+  }
+}
+
+for (let i = 0; i < category.length; i++) {
+  category[i].addEventListener("click", showMenu);
+}
