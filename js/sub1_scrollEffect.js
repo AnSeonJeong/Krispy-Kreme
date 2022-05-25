93936bd98a8aaa5b5a6933d6204cbcba689a872a
AnSeonJeong <asj081@naver.com>
2022-05-25 21:27:01 +0900

    svg_animation

--- js/sub1_scrollEffect.js
+++ js/sub1_scrollEffect.js
@@ -3,16 +3,33 @@ const storyWrap = document.querySelectorAll(".story_box .story_wrap");
 const storyDesc = document.querySelectorAll(".story_box .story_desc");
 const shareTheLove = document.getElementById("share_the_love");
 const shareTheLoveList = shareTheLove.querySelectorAll("ul li");
+const crs = document.getElementById("crs");
+const crsPath = document.querySelectorAll("#crs path");
+const dozenBox = document.getElementById("dozen_box");
+const dozenPath = document.querySelectorAll("#dozen_box path");
+
+for (let i = 0; i < crsPath.length; i++) {
+  crsPath[i].style.strokeDasharray = crsPath[i].getTotalLength();
+  crsPath[i].style.strokeDashoffset = crsPath[i].getTotalLength();
+}
+
+for (let i = 0; i < dozenPath.length; i++) {
+  dozenPath[i].style.strokeDasharray = dozenPath[i].getTotalLength();
+  dozenPath[i].style.strokeDashoffset = dozenPath[i].getTotalLength();
+}
 
 const ODD = "odd";
 const EVEN = "even";
+const FILL = "fill";
+const DRAW = "draw";
 
 let storyPosArr = [];
 for (let i = 0; i < storyCon.length; i++) {
   const storyPos = storyCon[i].offsetTop - 700;
   storyPosArr.push(storyPos);
 }
-const storyConWidth = storyCon[0].getBoundingClientRect().width + 230;
+
+const storyConWidth = storyCon[0].getBoundingClientRect().width + 260;
 const STLHeight = shareTheLoveList[0].getBoundingClientRect().height + 170;
 const STLPos = shareTheLove.offsetTop - 500;
 
@@ -45,7 +62,7 @@ window.addEventListener("scroll", () => {
   const currentScroll = document.body.scrollTop
     ? document.body.scrlollTop
     : document.documentElement.scrollTop;
-
+  console.log(currentScroll);
   function originalPos(num, name, i) {
     if (currentScroll > storyPosArr[num]) {
       storyCon[num].style.transform = `translateX(0px)`;
@@ -58,10 +75,25 @@ window.addEventListener("scroll", () => {
     }
   }
 
+  function drawText(svg, path, num, name1, name2) {
+    for (let i = 0; i < path.length; i++) {
+      if (currentScroll > storyPosArr[num]) {
+        path[i].classList.add(name1);
+        svg.classList.add(name2);
+      } else {
+        path[i].classList.remove(name1);
+        svg.classList.remove(name2);
+      }
+    }
+  }
+
   originalPos(0, ODD, -1);
   originalPos(1, EVEN, 1);
   originalPos(2, ODD, -1);
   originalPos(3, EVEN, 1);
+
+  drawText(crs, crsPath, 2, DRAW, FILL);
+  drawText(dozenBox, dozenPath, 3, DRAW, FILL);
 
   if (currentScroll > STLPos) {
     for (let i = 0; i < shareTheLoveList.length; i++) {
